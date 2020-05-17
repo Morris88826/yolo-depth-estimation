36d26e78503e7ea4280239679aa4caf94f232f1d
Morris88826 <morris88826@gmail.com>
2020-05-17 16:03:40 +0800

    defined batch_size

--- yolov3/proj_code/layers.py
+++ yolov3/proj_code/layers.py
@@ -23,8 +23,8 @@ def Conv2D_Block(x, filters, kernel_size, strides=1, batch_norm=True):
         x = tf.keras.layers.LeakyReLU(alpha=0.1)(x)
     return x
 
-def Convolution(name=None):
-    x = inputs = tf.keras.layers.Input([416, 416, 3])
+def Convolution(batch_size=None, name=None):
+    x = inputs = tf.keras.layers.Input([416, 416, 3], batch_size=batch_size)
     x = Conv2D_Block(x, 16, 3)
     x = tf.keras.layers.MaxPool2D(2, 2, 'same')(x)
     x = Conv2D_Block(x, 32, 3)
@@ -41,16 +41,17 @@ def Convolution(name=None):
     return tf.keras.Model(inputs, (x_8, x), name=name)
 
 
-def YoloConv(filters, name=None):
+def YoloConv(filters, batch_size = None, name=None):
     def yolo_conv(x_in):
         if isinstance(x_in, tuple):
-            inputs = tf.keras.layers.Input(x_in[0].shape[1:]), tf.keras.layers.Input(x_in[1].shape[1:])
+            print(batch_size)
+            inputs = tf.keras.layers.Input(x_in[0].shape[1:], batch_size=batch_size), tf.keras.layers.Input(x_in[1].shape[1:], batch_size=batch_size)
             x, x_skip = inputs
-
             # concat with skip connection
             x = Conv2D_Block(x, filters, 1)
             x = tf.keras.layers.UpSampling2D(2)(x)
             x = tf.keras.layers.Concatenate()([x, x_skip])
+            print(x.shape)
         else:
             x = inputs = tf.keras.layers.Input(x_in.shape[1:])
             x = Conv2D_Block(x, filters, 1)
@@ -58,9 +59,9 @@ def YoloConv(filters, name=None):
         return tf.keras.Model(inputs, x, name=name)(x_in)
     return yolo_conv
 
-def YoloOutput(filters, anchors, classes, name=None):
+def YoloOutput(filters, anchors, classes, batch_size=None, name=None):
     def yolo_output(x_in):
-        x = inputs = tf.keras.layers.Input(x_in.shape[1:])
+        x = inputs = tf.keras.layers.Input(x_in.shape[1:], batch_size=batch_size)
         x = Conv2D_Block(x, filters * 2, 3)
         x = Conv2D_Block(x, anchors * (classes + 5), 1, batch_norm=False)
         # x = tf.keras.layers.Lambda(lambda x: tf.reshape(x, (-1, tf.shape(x)[1], tf.shape(x)[2],
@@ -68,18 +69,20 @@ def YoloOutput(filters, anchors, classes, name=None):
         return tf.keras.Model(inputs, x, name=name)(x_in)
     return yolo_output
 
-def YoloV3_Tiny(size=None, channels=3, anchors=yolo_tiny_anchors,
+def YoloV3_Tiny(input_shape=None, channels=3, anchors=yolo_tiny_anchors,
                masks=yolo_tiny_anchor_masks, classes=80, score_threshold = 0.5, iou_threshold = 0.5, resolution=416):
-               
-    x = inputs = tf.keras.layers.Input([size, size, channels], name='input')
+    
+    batch_size, size, _, channels = input_shape
+    # batch_size = None
+    x = inputs = tf.keras.layers.Input([size, size, channels], name='input', batch_size=batch_size)
 
-    x_8, x = Convolution(name='convolution')(x)
+    x_8, x = Convolution(batch_size=batch_size, name='convolution')(x)
 
-    x = YoloConv(256, name='yolo_conv_0')(x)
-    output_0 = YoloOutput(256, len(masks[0]), classes, name='yolo_output_0')(x)
+    x = YoloConv(256, batch_size=batch_size, name='yolo_conv_0')(x)
+    output_0 = YoloOutput(256, len(masks[0]), classes, batch_size=batch_size, name='yolo_output_0')(x)
 
-    x = YoloConv(128, name='yolo_conv_1')((x, x_8))
-    output_1 = YoloOutput(128, len(masks[1]), classes, name='yolo_output_1')(x)
+    x = YoloConv(128, batch_size=batch_size, name='yolo_conv_1')((x, x_8))
+    output_1 = YoloOutput(128, len(masks[1]), classes, batch_size=batch_size, name='yolo_output_1')(x)
 
 
     boxes_0 = tf.keras.layers.Lambda(lambda x: bbox_prediction(x, np.array([(81, 82), (135, 169),  (344, 319)]), classes, resolution),
@@ -87,33 +90,37 @@ def YoloV3_Tiny(size=None, channels=3, anchors=yolo_tiny_anchors,
     boxes_1 = tf.keras.layers.Lambda(lambda x: bbox_prediction(x, np.array([(10, 14), (23, 27), (37, 58)]), classes, resolution),
                      name='yolo_boxes_1')(output_1)
     
-    outputs = tf.concat([boxes_0, boxes_1], axis=-1)
+    outputs = tf.concat([boxes_0, boxes_1], axis=1)
     # print(outputs.shape)
     # outputs = tf.keras.layers.Lambda(lambda x: non_maximum_suppression(x, score_threshold, iou_threshold),
     #                  name='yolo_nms')((boxes_0[:3], boxes_1[:3]))
-    return tf.keras.Model(inputs, outputs, name='yolov3_tiny')
+    return tf.keras.Model(inputs, boxes_0, name='yolov3_tiny')
 
 
-def testing(size=None, channels=3, anchors=yolo_tiny_anchors,
+def testing(input_shape=None, channels=3, anchors=yolo_tiny_anchors,
                masks=yolo_tiny_anchor_masks, classes=80, score_threshold = 0.5, iou_threshold = 0.5):
-               
-    x = inputs = tf.keras.layers.Input([size, size, channels], name='input')
+    
+    batch_size, height, width, channels = input_shape
 
-    x_8, x = Convolution(name='convolution')(x)
-    x = YoloConv(256, name='yolo_conv_0')(x)
-    output_0 = YoloOutput(256, len(masks[0]), classes, name='yolo_output_0')(x)
+    batch_size = None
 
-    x = YoloConv(128, name='yolo_conv_1')((x, x_8))
-    output_1 = YoloOutput(128, len(masks[1]), classes, name='yolo_output_1')(x)
+    x = inputs = tf.keras.layers.Input([height, width, channels], name='input', batch_size=batch_size)
+
+    x_8, x = Convolution(batch_size=batch_size, name='convolution')(x)
+    x = YoloConv(256, batch_size=batch_size, name='yolo_conv_0')(x)
+    output_0 = YoloOutput(256, len(masks[0]), classes, batch_size=batch_size, name='yolo_output_0')(x)
+
+    x = YoloConv(128, batch_size=batch_size, name='yolo_conv_1')((x, x_8))
+    output_1 = YoloOutput(128, len(masks[1]), classes, batch_size=batch_size, name='yolo_output_1')(x)
 
 
-    boxes_0 = tf.keras.layers.Lambda(lambda x: bbox_prediction(x, np.array([(81, 82), (135, 169),  (344, 319)]), classes, size),
+    boxes_0 = tf.keras.layers.Lambda(lambda x: bbox_prediction(x, np.array([(81, 82), (135, 169),  (344, 319)]), classes, height),
                      name='yolo_boxes_0')(output_0)
 
-    boxes_1 = tf.keras.layers.Lambda(lambda x: bbox_prediction(x, np.array([(10, 14), (23, 27), (37, 58)]), classes, size),
+    boxes_1 = tf.keras.layers.Lambda(lambda x: bbox_prediction(x, np.array([(10, 14), (23, 27), (37, 58)]), classes, height),
                      name='yolo_boxes_1')(output_1)
 
     
     # outputs = tf.keras.layers.Concatenate(axis=1)([boxes_0, boxes_1])
 
-    return tf.keras.Model(inputs, boxes_0, name="yolo")
+    return tf.keras.Model(inputs, output_1, name="yolo")
